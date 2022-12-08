f0f0ff90b559776e5805245a674a3f9485ad0f77
Lisk809 <120005968+Lisk809@users.noreply.github.com>
2022-12-08 22:55:27 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 exports.plugin = void 0;
 const core_1 = require("@kivibot/core");
 const plugin = new core_1.KiviPlugin('来张图', '1.0.0');
 const https = require('https')
 exports.plugin = plugin;
 const config = {
-  setu:1,
-  //发送模式，是否有涩图
+    "setu": 1,
+    //发送模式，是否有涩图
+    "mode": 1
+    //图片类型，0为壁纸模式，1为随机
+    "wallpaper_type": ""
+    //当为壁纸模式的时候的壁纸类型，mp为竖屏，pc为横屏
 };
 plugin.onMounted(() => {
-    if(setu==1){
-        plugin.onMatch('来张图', (event)  => {
-            event.reply("https://iw233.cn/api.php?sort=random",false)
-  })
-    
+    if (config["mode"] == 1) {
+        if (config["setu"] == 1) {
+            plugin.onMatch('来张图', (event) => {
+                event.reply(cqcode.image("https://iw233.cn/api.php?sort=random", false))
+            })
+
+        } else {
+            plugin.onMatch('来张图', (event) => {
+                event.reply(cqcode.image("https://iw233.cn/api.php?sort=iw233", false))
+
+            })
+        }
+    } else {
+        event.reply(cqcode.image("https://iw233.cn/api.php?sort=" + config["wallpaper_type"], false))
+
+
+
     }
-    else{
-        plugin.onMatch('来张图', (event)  => {
-            event.reply("https://iw233.cn/api.php?sort=iw233",false)
-    
-    }
-    }});
+});
